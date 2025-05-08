6e933b1522916a8d27e157f71a2087f1d695007e
u <u>
2025-05-08 10:25:25 +0300

    update

--- file.pl
+++ file.pl
@@ -1,6 +1,6 @@
 if ($. == 1){
-  $_ = "\n$_\nBuildRequires: gcc\nBuildRequires: libffi-devel\n%define debug_package %{nil}\n";
+  $_ = "\n$_\nBuildRequires: gcc\nBuildRequires: sed\nBuildRequires: libffi-devel\n%define debug_package %{nil}\n";
 }
 s~^Summary:.*~Summary: Access Objective-C classes from Python~g;
 s~^URL:.*~URL: pyobjus.readthedocs.io~g;
-s~(%files.*)~\1\n%{_datadir}/pyobjus-examples~
+s~(%pyproject_save_files.*)~\n\1\nrm -rfv %{buildroot}%{_datadir} ||: ; sed -i 's#%{_datadir}.*##g;' %{pyproject_files}\n~g;
