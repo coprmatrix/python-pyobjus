56b6a6ee28bb80321a0a64255a0da6f230c9e0c5
u <u>
2025-05-08 09:29:28 +0300

    update

--- file.pl
+++ file.pl
@@ -1,2 +1,5 @@
+if ($. == 1){
+  $_ = "\n$_\nBuildRequires: gcc\nBuildRequires: libffi-devel\n%define debug_package %{nil}\n";
+}
 s~^Summary:.*~Summary: Access Objective-C classes from Python~g;
 s~^URL:.*~URL: pyobjus.readthedocs.io~g;
